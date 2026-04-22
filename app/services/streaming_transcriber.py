ff48303fad4de035a18a440adacef24758dc064d
Swayam Jethi <swayam.jethi@gmail.com>
2026-04-23 00:15:39 +0530

    Fix: Add speech_model parameter to StreamingParameters

--- app/services/streaming_transcriber.py
+++ app/services/streaming_transcriber.py
@@ -47,7 +47,7 @@ class AssemblyAIStreamingTranscriber:
         self.client.on(StreamingEvents.Termination, on_termination)
         self.client.on(StreamingEvents.Error, on_error)
         self.client.connect(StreamingParameters(
-            sample_rate=sample_rate, format_turns=True))
+            sample_rate=sample_rate, format_turns=True, speech_model="best"))
     def stream_audio(self, audio_chunk: bytes):
         self.client.stream(audio_chunk)
     def close(self):
